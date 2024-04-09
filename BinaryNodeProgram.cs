6a407d66114f2a9e52e45c66c7f2108100991cf4
FOXDIE0 <86891826+FOXDIE0@users.noreply.github.com>
2024-04-09 13:31:12 -0600

    Update BinaryNodeProgram.cs
    add comments

--- BinaryNodeProgram.cs
+++ BinaryNodeProgram.cs
@@ -1,5 +1,6 @@
 ﻿using binary_node1;
 
+// create nodes
 BinaryNode<string> rootNode = new BinaryNode<string> ("test");
 BinaryNode<string> nodeA = new BinaryNode<string>("A");
 BinaryNode<string> nodeB = new BinaryNode<string>("B");
@@ -7,6 +8,8 @@ BinaryNode<string> nodeC = new BinaryNode<string>("C");
 BinaryNode<string> nodeD = new BinaryNode<string>("D");
 BinaryNode<string> nodeE = new BinaryNode<string>("E");
 BinaryNode<string> nodeF = new BinaryNode<string>("F");
+
+// add children to nodes
 rootNode.AddLeft(nodeA);
 rootNode.AddRight(nodeB);
 nodeA.AddLeft(nodeC);
@@ -14,6 +17,7 @@ nodeA.AddRight(nodeD);
 nodeB.AddRight(nodeE);
 nodeE.AddLeft(nodeF);
 
+// print nodes
 Console.WriteLine(rootNode.ToString());
 Console.WriteLine(nodeA.ToString());
 Console.WriteLine(nodeB.ToString());
